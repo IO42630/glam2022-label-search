020a09f546524e5caaacc4b89939596373b330b7
io42630 <ivan@olexyn.com>
2022-11-04 21:06:47 +0100

    + add connector

---
+++ escriptorium-connector.py
@@ -0,0 +1,12 @@
+from escriptorium_connector import EscriptoriumConnector
+import os
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    load_dotenv()
+    url = str(os.getenv('ESCRIPTORIUM_URL'))
+    username = str(os.getenv('ESCRIPTORIUM_USERNAME'))
+    password = str(os.getenv('ESCRIPTORIUM_PASSWORD'))
+    escr = EscriptoriumConnector(url, username, password)
+    print(escr.get_documents())
+    br = 0
--- tesseract.py
+++ tesseract.py
@@ -4,6 +4,6 @@ tesser = 'C:/Progra~1/Tesseract-OCR/tesseract'
 images = 'C:/Users/User/home/ws/glam2022-label-search/data/images'
 for file in os.listdir(images):
     source = images + '/' + file
-    target = source.replace('images', 'text').replace('.jpg', '.txt')
+    target = source.replace('images', 'text').replace('.jpg', '')
     cmd = tesser + ' ' + source + ' ' + target
     os.system(command=cmd)
