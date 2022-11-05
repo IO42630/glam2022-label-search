aa5c9159c08254ad80d032e59c35613d2ba26e07
io42630 <ivan@olexyn.com>
2022-11-05 13:20:58 +0100

    + push the diff

--- Cropping/Cropping.py
+++ Cropping/Cropping.py
@@ -1,7 +1,7 @@
-#python 3.10
-#Reducing uploading time in escriptorium
-#usually the size of the image is about 9mb through the cropping its about 4mb
-#crop each image of the folder (only 7 jpegs) and save it in another folder
+# python 3.10
+# Reducing uploading time in escriptorium
+# usually the size of the image is about 9mb through the cropping its about 4mb
+# crop each image of the folder (only 7 jpegs) and save it in another folder
 from PIL import Image
 import os
 
@@ -9,18 +9,21 @@ path = "./file/pathname/"
 savepath = "./file/pathname/"
 dirs = os.listdir(path)
 
+
 def crop():
     for item in dirs:
-        if os.path.isfile(path+item):
-            im = Image.open(path+item)
-            f, e = os.path.splitext(path+item)
-            imCrop = im.crop((10, 6200, 6000, 6500)) #left, upper, right, lower
+        if os.path.isfile(path + item):
+            im = Image.open(path + item)
+            f, e = os.path.splitext(path + item)
+            imCrop = im.crop((10, 6200, 6000, 6500))  # left, upper, right, lower
             # save in other folder and rename the file to the original name with _cropped.jpg
             imCrop.save(savepath + item + '_cropped.jpg', 'JPEG', quality=100)
-crop()
-#thanks to the inspiration of sanjeev2552
-#https://www.geeksforgeeks.org/python-pil-image-crop-method/
 
-#next steps would be to extand the code for cropping more images,
-#but the size is different
-#so idea would be to add create a loop: estimate the size of the image, cropt it and go to the next image
+
+crop()
+# thanks to the inspiration of sanjeev2552
+# https://www.geeksforgeeks.org/python-pil-image-crop-method/
+
+# next steps would be to extand the code for cropping more images,
+# but the size is different
+# so idea would be to add create a loop: estimate the size of the image, cropt it and go to the next image
