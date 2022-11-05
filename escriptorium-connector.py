dfa6d440aa7b384bebae6e297845a6f0a52a1e10
Rae Knowler <rae.knowler@liip.ch>
2022-11-05 14:46:30 +0100

    Try out things with escriptorium connector

--- escriptorium-connector.py
+++ escriptorium-connector.py
@@ -1,6 +1,14 @@
+# coding: utf-8
 from escriptorium_connector import EscriptoriumConnector
 import os
 from dotenv import load_dotenv
+from pprint import pprint
+
+# Hardcoding these for now
+PROJECT_NAME = '1538_jpges_WALO_Tryagain'
+DOC_NAME = 'PackageAfterPackage'
+DOC_PK = 7
+
 
 if __name__ == '__main__':
     load_dotenv()
@@ -8,7 +16,12 @@ if __name__ == '__main__':
     username = str(os.getenv('ESCRIPTORIUM_USERNAME'))
     password = str(os.getenv('ESCRIPTORIUM_PASSWORD'))
     escr = EscriptoriumConnector(url, username, password)
-    print(escr.get_documents())
-    escr.g
-    br = 0
-# by the way escriptorium is using https://kraken.re/master/index.html
+
+    # pprint(escr.get_projects())
+
+    project_pk = escr.get_project_pk_by_name(PROJECT_NAME)
+    project = escr.get_project(project_pk)
+
+    transcriptions = escr.get_document_transcriptions(DOC_PK)
+    for t in transcriptions:
+        print(t)
